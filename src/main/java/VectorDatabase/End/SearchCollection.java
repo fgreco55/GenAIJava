dba7c5c5751c64572456e83f38c695ccdf0185b3
Frank Greco <fgreco@javasig.com>
2024-04-23 21:46:58 -0400

    Remove the main method

--- src/main/java/VectorDatabase/End/SearchCollection.java
+++ src/main/java/VectorDatabase/End/SearchCollection.java
@@ -167,23 +167,4 @@ public class SearchCollection {
 
         return searchDB_using_targetvectors(coll, smallvec, max);   // Give me at most max best matches
     }
-
-    /*public static void main(String[] args) throws IOException {
-        SearchCollection cc = new SearchCollection();
-
-        cc.setMc(cc.connectToMilvus("localhost", 19530));      // Connect to the VDB
-
-        String token = Misc.getAPIkey();                                // Connect to OpenAI
-        cc.setService(new OpenAiService(token));
-
-        String collection = "frank5";
-        cc.loadCollection(collection);
-
-        List<String> matches;
-        matches = cc.searchDB(collection, "jdk 22", 5);
-        System.out.println("MATCHES ------------------------");
-        for (String m: matches) {
-            System.out.println("MATCH: [" + m + "]");
-        }
-    }*/
 }
