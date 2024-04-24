dfed1e0911a7beb47f7a68b932deca43e7b6a761
Frank Greco <fgreco@javasig.com>
2024-04-23 20:51:19 -0400

    CreateCollectionApp now has simple app that exercises the methods in CreateCollection.

---
+++ src/main/java/VectorDatabase/End/CreateCollectionApp.java
@@ -0,0 +1,21 @@
+package VectorDatabase.End;
+
+import Utilities.Misc;
+import com.theokanning.openai.service.OpenAiService;
+
+import java.io.IOException;
+
+public class CreateCollectionApp {
+    public static void main(String[] args) throws IOException {
+        CreateCollection cc = new CreateCollection();
+
+        cc.setMc(cc.connectToMilvus("localhost", 19530));      // Connect to the VDB
+
+        String token = Misc.getAPIkey();                                // Connect to OpenAI
+        cc.setService(new OpenAiService(token));
+
+        int ver = 3;
+        cc.createCollection("frank" + ver, "This is test " + ver + " of my create-collection example");
+        cc.insert_file("frank" + ver, "./src/main/resources/mydata.txt");
+    }
+}
