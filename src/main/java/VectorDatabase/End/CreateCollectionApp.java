a1896aeab01abc1dfc70b4d4ea6e1169fd443952
Frank Greco <fgreco@javasig.com>
2024-04-27 12:41:09 -0400

    Inserts a 2nd data file.

--- src/main/java/VectorDatabase/End/CreateCollectionApp.java
+++ src/main/java/VectorDatabase/End/CreateCollectionApp.java
@@ -16,6 +16,9 @@ public class CreateCollectionApp {
 
         int ver = 5;
         cc.createCollection("frank" + ver, "This is test " + ver + " of my create-collection example");
+        System.out.println("Inserting mydata.txt");
         cc.insert_file("frank" + ver, "./src/main/resources/mydata.txt");
+        System.out.println("Inserting java22.txt");
+        cc.insert_file("frank" + ver, "./src/main/resources/java22.txt");
     }
 }
