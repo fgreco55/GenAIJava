19a6b30726389e83e82ebd109302b87a451a2b06
Frank Greco <fgreco@javasig.com>
2024-04-27 12:42:21 -0400

    file is now formatted better.  No significant code changes.

--- src/main/java/Chatbot/End/ChatApp.java
+++ src/main/java/Chatbot/End/ChatApp.java
@@ -1,27 +1,28 @@
 package Chatbot.End;
 
 import Utilities.Misc;
+
 import java.io.IOException;
 import java.util.Scanner;
 
 public class ChatApp {
     public static void main(String[] args) throws IOException {
-            Scanner userinput;                                  // user inputted line as a Scanner
-            String cmdline;
+        Scanner userinput;                                  // user inputted line as a Scanner
+        String cmdline;
 
-            String token = Misc.getAPIkey();                    // API key for the cloud GenAI REST service
+        String token = Misc.getAPIkey();                    // API key for the cloud GenAI REST service
 
-            Chatbot cb = new Chatbot(token);
+        Chatbot cb = new Chatbot(token);
 
-            while (true) {
+        while (true) {
 
-                userinput = new Scanner(System.in);
-                System.out.print("prompt> ");
-                cmdline = userinput.nextLine();
+            userinput = new Scanner(System.in);
+            System.out.print("prompt> ");
+            cmdline = userinput.nextLine();
 
-                if (cmdline.isEmpty())
-                    continue;
-                System.out.println(cb.getCompletion(cmdline));
-            }
+            if (cmdline.isEmpty())
+                continue;
+            System.out.println(cb.getCompletion(cmdline));
         }
+    }
 }
