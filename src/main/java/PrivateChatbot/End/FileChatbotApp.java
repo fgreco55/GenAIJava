76b254d8cc94b185061cfb7ccbe352f21bd9ebd0
Frank Greco <fgreco@javasig.com>
2024-05-17 16:00:05 -0400

    Added FileChatbot.java and FileChatbotApp.java example. This exercise just illustrates what a very primitive RAG system is doing.

---
+++ src/main/java/PrivateChatbot/End/FileChatbotApp.java
@@ -0,0 +1,33 @@
+package PrivateChatbot.End;
+
+import Utilities.Misc;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class FileChatbotApp {
+    public static void main(String[] args) throws IOException {
+        Scanner userinput;                                  // user inputted line as a Scanner
+        String cmdline;
+
+        String token = Misc.getAPIkey();                    // API key for the cloud GenAI REST service
+
+        String folder = "src/main/resources/";
+        List<String> myfiles = Arrays.asList(folder + "java22.txt", folder + "mydata.txt");
+        String inst = "You are a extremely helpful Java expert and will respond as one";
+        
+        FileChatbot pcb = new FileChatbot(token, myfiles, inst);
+
+        while (true) {
+            userinput = new Scanner(System.in);
+            System.out.print("prompt> ");
+            cmdline = userinput.nextLine();
+
+            if (cmdline.isEmpty())
+                continue;
+            System.out.println(pcb.getCompletion(cmdline));
+        }
+    }
+}
