f68c9f4e4634826b725183f2eae69b2dd70199ec
Frank Greco <fgreco@javasig.com>
2024-07-20 11:22:40 -0400

    Now allows more flexibility in naming the collection and get that info from the user.

--- src/main/java/VectorDatabase/End/CreateCollectionApp.java
+++ src/main/java/VectorDatabase/End/CreateCollectionApp.java
@@ -3,21 +3,25 @@ package VectorDatabase.End;
 import Utilities.Misc;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 public class CreateCollectionApp {
     public static void main(String[] args) throws IOException {
         CreateCollection cc = new CreateCollection(Misc.getAPIkey());
+        Scanner userinput;
+        String collname;
+        String username = Misc.getUser();
 
         cc.setMc(cc.connectToMilvus("localhost", 19530));      // Connect to the VDB
 
-        String collprefix = "frank";
-        int ver = 5;
-        String coll = collprefix + ver;     // Just a naming convenience... collection names can be any string
+        userinput = new Scanner(System.in);
+        System.out.print("collection to create> ");
+        collname = userinput.nextLine();
 
-        cc.createCollection(coll, "This is test " + ver + " of my create-collection example");
+        cc.createCollection(collname, "Created by " + username + " from CreateCollectionApp.java");
         System.out.println("**********Inserting mydata.txt");
-        cc.insert_file(coll, "./src/main/resources/mydata.txt");
+        cc.insert_file(collname, "./src/main/resources/mydata.txt");
         System.out.println("**********Inserting java22.txt");
-        cc.insert_file(coll, "./src/main/resources/java22.txt");
+        cc.insert_file(collname, "./src/main/resources/java22.txt");
     }
 }
